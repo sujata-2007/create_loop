a5bf348dc750eac32dbc5adbda1393feff8ef312
Sujata Biswas <sujatabiswas20007@gmail.com>
2026-09-08 10:43:48 +0530

    Add C program for right-aligned triangle pattern

---
+++ right_aligned_triangle pattern.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,a, num=1;
+    printf("enter the rows:");
+    scanf("%d",&a);
+    
+    for(i=1; i<=a; i++)
+    {
+        for(j=i; j<=a; j++)
+    {
+    printf(" ");
+    }
+    for(j=1; j<=i; j++)
+    {
+        printf("*");
+    }
+    printf("\n");
+    }
+  return 0;  
+}
