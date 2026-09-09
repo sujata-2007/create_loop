bed5c6dc0ea19b1b49b2ac545625846f43bfa67c
Sujata Biswas <sujatabiswas20007@gmail.com>
2026-09-09 21:58:38 +0530

    Add diamond pattern printing program in C

---
+++ diamond_pattern.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+    int i,j, a,num=1;
+    printf("enter the rows:");
+    scanf("%d",&a);
+    for(i=1;i<=a; i++)
+    {
+        for(j=i; j<=a; j++)
+        {
+         printf(" ");
+    }
+    for(j=1; j<=i; j++)
+    {
+        printf("+ ");
+    }
+    printf("\n");
+    }
+    
+    for(i=1;i<=a; i++)
+    {
+        for(j=1; j<=i; j++)
+        {
+         printf(" ");
+    }
+    for(j=i; j<=a-1; j++)
+    {
+        printf(" +");
+    }
+    printf("\n");
+    }
+return 0;
+}
